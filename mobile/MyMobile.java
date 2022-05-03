a84eaaeeb964623f0f43d1475f5094b83175a389
jeyakishansd <94844789+jeyakishansd@users.noreply.github.com>
2022-05-03 16:09:35 +0530

    Add files via upload

---
+++ mobile/MyMobile.java
@@ -0,0 +1,12 @@
+package week1.mobile;
+
+import week1.day2.Mobile;
+
+public class MyMobile {
+public static void main(String[] args) {
+	Mobile m = new Mobile();
+	m.unlock();
+	m.openCamera();
+	m.deletePicture();
+}
+}
